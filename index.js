da7acba2a600fadf8353c102e824875eaafc4ffb
shahed154 <shahedadnan@hotmail.com>
2025-04-13 21:32:33 -0400

    added mmore files i think i will use

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//const logger = require('./middleware/logger');
-//const errorHandler = require('./middleware/errorHandler');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandling')
+
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/users', require('./routes/users'))
+app.use('/api/listings', require('./routes/listings'));
+app.use('/api/categories', require('./routes/categories'));
+
 
 app.get('', (req,res) => 
     {
